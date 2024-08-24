d078e17d98f4aeea2c390db6c987d124adc9f125
Girl DoCode <girldocode@gmail.com>
2024-08-24 09:28:47 +0500

    refactoring

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,13 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Inter, Roboto } from "next/font/google";
 import "../styles/global.scss";
+
 const inter = Inter({ subsets: ["latin"] });
+const roboto = Roboto({
+  subsets: ["latin"],
+  weight: ["100", "300", "400", "500", "700", "900"],
+  style: ["normal", "italic"],
+});
 
 export const metadata: Metadata = {
   title: "Weeb",
@@ -15,7 +21,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className} ${roboto.className}`}>
+        {children}
+      </body>
     </html>
   );
 }
